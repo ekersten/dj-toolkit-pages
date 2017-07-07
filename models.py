c2cf2bb12ea99cd1d44e1d0a36a8bd53b093c3ba
Eric Kersten <eric.kersten@egoargentina.com>
2017-07-07 17:26:10 -0300

    initial page model & admin

--- models.py
+++ models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
+from tags.models import Taggable
+from core.models import Shareable
+from core.models import Publishable
+from core.models import Approveable
 
 # Create your models here.
+
+class Page(TimeStampedModel, Taggable, Shareable, Approveable, Publishable):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
